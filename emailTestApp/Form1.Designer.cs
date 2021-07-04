d1a4f06fb4910ff8ed595132345a914c9415c3df
jovanmhn <jovanmhn@gmail.com>
2021-07-04 13:47:36 +0200

    Add project files.

---
+++ emailTestApp/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace emailTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.checkBoxSSL = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.checkBoxShow = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxRecipient = new System.Windows.Forms.TextBox();
+            this.textBoxResponse = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(133, 51);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(176, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(133, 103);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(176, 20);
+            this.textBoxServer.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(133, 77);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(176, 20);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // checkBoxSSL
+            // 
+            this.checkBoxSSL.AutoSize = true;
+            this.checkBoxSSL.Checked = true;
+            this.checkBoxSSL.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSSL.Location = new System.Drawing.Point(133, 156);
+            this.checkBoxSSL.Name = "checkBoxSSL";
+            this.checkBoxSSL.Size = new System.Drawing.Size(46, 17);
+            this.checkBoxSSL.TabIndex = 4;
+            this.checkBoxSSL.Text = "SSL";
+            this.checkBoxSSL.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(286, 321);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(69, 25);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "email user";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "email pass";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "email server";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "email server port";
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(133, 130);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown.TabIndex = 11;
+            this.numericUpDown.Value = new decimal(new int[] {
+            587,
+            0,
+            0,
+            0});
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Location = new System.Drawing.Point(133, 180);
+            this.textBoxContent.Multiline = true;
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(225, 138);
+            this.textBoxContent.TabIndex = 12;
+            this.textBoxContent.Text = "Hello!";
+            // 
+            // checkBoxShow
+            // 
+            this.checkBoxShow.AutoSize = true;
+            this.checkBoxShow.Location = new System.Drawing.Point(315, 79);
+            this.checkBoxShow.Name = "checkBoxShow";
+            this.checkBoxShow.Size = new System.Drawing.Size(51, 17);
+            this.checkBoxShow.TabIndex = 13;
+            this.checkBoxShow.Text = "show";
+            this.checkBoxShow.UseVisualStyleBackColor = true;
+            this.checkBoxShow.CheckedChanged += new System.EventHandler(this.checkBoxShow_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Send to";
+            // 
+            // textBoxRecipient
+            // 
+            this.textBoxRecipient.Location = new System.Drawing.Point(104, 324);
+            this.textBoxRecipient.Name = "textBoxRecipient";
+            this.textBoxRecipient.Size = new System.Drawing.Size(176, 20);
+            this.textBoxRecipient.TabIndex = 14;
+            // 
+            // textBoxResponse
+            // 
+            this.textBoxResponse.Location = new System.Drawing.Point(399, 51);
+            this.textBoxResponse.Multiline = true;
+            this.textBoxResponse.Name = "textBoxResponse";
+            this.textBoxResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResponse.Size = new System.Drawing.Size(337, 347);
+            this.textBoxResponse.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 450);
+            this.Controls.Add(this.textBoxResponse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxRecipient);
+            this.Controls.Add(this.checkBoxShow);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.checkBoxSSL);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.textBoxUsername);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.CheckBox checkBoxSSL;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.TextBox textBoxContent;
+        private System.Windows.Forms.CheckBox checkBoxShow;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxRecipient;
+        private System.Windows.Forms.TextBox textBoxResponse;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ emailTestApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emailTestApp
+{
+    public partial class Form1 : Form
+    {
+        //static bool mailSent = false;
+        public string someArrows = new string(new char[] { '\u2190', '\u2191', '\u2192', '\u2193' });
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+                btnSend.Enabled = false;
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = textBoxServer.Text;
+                smtpClient.Port = (int)numericUpDown.Value;
+                smtpClient.EnableSsl = checkBoxSSL.Checked;
+                smtpClient.Credentials = new System.Net.NetworkCredential(textBoxUsername.Text, textBoxPassword.Text);
+                smtpClient.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
+
+                MailMessage mail = new MailMessage();
+                mail.Subject = "TEST APP";
+                mail.Body = textBoxContent.Text;
+                mail.From = new MailAddress(textBoxUsername.Text, "TEST APP", Encoding.UTF8);
+                mail.To.Add(new MailAddress(textBoxRecipient.Text));
+                string token = "token";
+                smtpClient.SendAsync(mail, token);
+                //textBoxResponse.Text += $"{DateTime.Now.ToString("dd.MM.yyyy. HH:mm:ss")} - Message sent." + System.Environment.NewLine + System.Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                textBoxResponse.Text += "["+DateTime.Now.ToString("dd.MM.yyyy. HH:mm:ss")+"] " + $"ERROR {ex.GetType().ToString()}:" + ex.Message + (ex.InnerException?.Message ?? "") + System.Environment.NewLine + System.Environment.NewLine;
+                btnSend.Enabled = true;
+            }
+        }
+
+        private void checkBoxShow_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = !checkBoxShow.Checked;
+        }
+        public void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
+
+            if (e.Cancelled)
+            {
+                textBoxResponse.Text +=  String.Format("[{0}] Send canceled.", token) + System.Environment.NewLine + System.Environment.NewLine;
+                btnSend.Enabled = true;
+            }
+            if (e.Error != null)
+            {
+                textBoxResponse.Text +=  String.Format ("[{1} - ERROR: {0}]", e.Error.ToString(), DateTime.Now.ToString("dd.MM.yyyy. HH:mm:ss")) + System.Environment.NewLine + System.Environment.NewLine;
+                btnSend.Enabled = true;
+            }
+            else
+            {
+                textBoxResponse.Text +=  $"[{DateTime.Now.ToString("dd.MM.yyyy. HH:mm:ss")}] - Message sent." + System.Environment.NewLine + System.Environment.NewLine;
+                btnSend.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxContent.Text += someArrows;
+        }
+    }
+}
